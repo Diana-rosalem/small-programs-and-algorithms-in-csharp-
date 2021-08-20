4db13187d76b82cd5893df0cd50f36dd889a1215
Diana <61847738+Diana-rosalem@users.noreply.github.com>
2021-08-20 15:58:33 -0300

    add grade program

---
+++ grade/Course.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Collections;
+
+
+
+class Curso
+{
+    public Curso(string nome)
+    {
+       
+        Nome = nome;
+        Notas = new ArrayList();
+       
+    }
+    
+
+    public string Nome { get; set; }
+    public ArrayList Notas { get; set; }
+  
+    public decimal MediaNotas()
+    {
+        decimal total = 0;
+        for (int i = 0; i < Notas.Count; i++)
+        {
+            total += (decimal)Notas[i];
+        }
+        return total / Notas.Count;
+    }
+
+    public decimal MaiorNota()
+    {
+        decimal maior = 0;
+        for (int i = 0; i < Notas.Count; i++)
+        {
+            if ((decimal)Notas[i] > maior)
+            {
+                maior = (decimal)Notas[i];
+            }
+        }
+        return maior;
+    }
+
+    public decimal MenorNota()
+    {
+        decimal menor = 11;
+        for (int i = 0; i < Notas.Count; i++)
+        {
+            if ((decimal)Notas[i] < menor)
+            {
+                menor = (decimal)Notas[i];
+            }
+        }
+        return menor;
+    }
+
+    public decimal Aprovados()
+    {
+        int aprovados = 0;
+        for (int i = 0; i < Notas.Count; i++)
+        {
+            if ((decimal)Notas[i] > 7)
+            {
+                aprovados++;
+            }
+        }
+        return aprovados;
+    }
+    public decimal Reprovados()
+    {
+        int reprovados = 0;
+        for (int i = 0; i < Notas.Count; i++)
+        {
+            if ((decimal)Notas[i] < 7)
+            {
+                reprovados++;
+            }
+        }
+        return reprovados;
+    }
+
+}
---
+++ grade/Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+
+
+
+class Program
+{
+    static void Main(string[] args)
+    {
+
+
+        Curso curso = new Curso("C#");
+
+        curso.Notas.Add((decimal)5.0);
+        curso.Notas.Add((decimal)6.0);
+        curso.Notas.Add((decimal)10.0);
+        curso.Notas.Add((decimal)9.0);
+        curso.Notas.Add((decimal)8.0);
+        curso.Notas.Add((decimal)2.0);
+
+
+
+
+        int op = 0;
+        while (op != 5)
+        {
+
+            Console.WriteLine("\n\n\t\t===== MENU =====");
+            Console.WriteLine("\tDigite 1 para calcular a media, maior nota e a menor nota");
+            Console.WriteLine("\tDigite 2 para calcular apenas a media");
+            Console.WriteLine("\tDigite 3 para calcular apenas a maior nota");
+            Console.WriteLine("\tDigite 4 para calcular apena a menor nota");
+            Console.WriteLine("\tDigite 5 para mostrar quantos alunos foram aprovados e reprovados ");
+            Console.WriteLine("\tDigite 6 para sair do programa");
+            Console.WriteLine("\tDigite sua escolha:");
+            op = Convert.ToInt32(Console.ReadLine());
+
+
+            switch (op) {
+                case 1:
+                    Console.WriteLine($"\n\n\t=== {curso.Nome} ===");
+                    Console.WriteLine($"\n\tMedia de {curso.Nome}= {curso.MediaNotas()}");
+                    Console.WriteLine($"\n \tnota de {curso.Nome}= {curso.MaiorNota()}");
+                    Console.WriteLine($"\n \tMenor nota de  {curso.Nome}= {curso.MenorNota()}");
+                    break;
+
+                case 2:
+                    Console.WriteLine($"\n\n\t=== {curso.Nome} ===");
+                    Console.WriteLine($"\n\tMedia de {curso.Nome}= {curso.MediaNotas()}");
+                    break;
+
+                case 3:
+                    Console.WriteLine($"\n\n\t=== {curso.Nome} ===");
+                    
+                    Console.WriteLine($"\n \tnota de {curso.Nome}= {curso.MaiorNota()}");
+                    break;
+
+                case 4:
+                    Console.WriteLine($"\n\n\t=== {curso.Nome} ===");
+                    Console.WriteLine($"\n \tMenor nota de  {curso.Nome}= {curso.MenorNota()}");
+                    break;
+                case 5:
+                    Console.WriteLine($"\n\n\t=== {curso.Nome} ===");
+                    Console.WriteLine($"\n \t Quantidade de alunos aprovados no curso de {curso.Nome}= {curso.Aprovados()}");
+                    Console.WriteLine($"\n \t Quantidade de alunos reprovados no curso de {curso.Nome}= {curso.Reprovados()}");
+                    break;
+            }
+           
+        }
+    }
+}
+
+
