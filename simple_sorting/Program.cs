49a7e8cb2d68d654fc2785d2920112cf49855927
Diana <61847738+Diana-rosalem@users.noreply.github.com>
2021-08-27 18:56:56 -0300

    add Insertionsort and Selectionsort
    Add Selectionsort and  Insertionsort code and a run menu

---
+++ simple_sorting/Program.cs
@@ -0,0 +1,138 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace simple_sorting
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int size = 100;
+            //Inicializar vetor
+            int[] vector1 = new int[size];
+            Random rand = new Random();
+            for (int i = 0; i < vector1.Length; i++)
+            {
+                vector1[i] = rand.Next(0, size);
+            }
+
+            int[] vector2 = new int[size];
+            vector1.CopyTo(vector2, 0);
+            int[] vector3 = new int[size];
+            vector1.CopyTo(vector3, 0);
+            int[] vector4 = new int[size];
+            vector1.CopyTo(vector4, 0);
+
+            Sorting sorting = new Sorting();
+
+
+            int op = 0;
+            while (op != 6)
+            {
+                Console.WriteLine("\n\n\t\t===== MENU =====");
+                Console.WriteLine("\tType 1 to sort the vector using Bubblesort,Insertionsort and Selectionsort methods.");
+                Console.WriteLine("\tType 2 to sort the vector using only the Bubblesort method.");
+                Console.WriteLine("\tType 3 to sort the vector using only the Insertionsort method.");
+                Console.WriteLine("\tType 4 to sort the vector using only the Selectionsort method.");
+                Console.WriteLine("\tType 5 to calculate the sorting time of the methods.");
+                Console.WriteLine("\tType 6 to exit the program.");
+                Console.WriteLine("\tType your choice:");
+                op = Convert.ToInt32(Console.ReadLine());
+
+                switch (op)
+                {
+                    case 1:
+                        //Sorting Bubblesort
+
+                        sorting.Bubblesort(vector1);
+
+                        //View
+                        Console.WriteLine("\n\tBubblesot:");
+                        foreach (var item in vector1)
+                        {
+                            Console.Write($"{item} - ");
+                        }
+
+                        //Sorting Insertionsort
+                        sorting.Insertionsort(vector2);
+
+                        //View
+                        Console.WriteLine("\n\tInsertionsort:");
+                        foreach (var item in vector2)
+                        {
+                            Console.Write($"{item} - ");
+                        }
+
+                        //Sorting Selectionsort
+                        sorting.Selectionsort(vector3);
+
+                        //View
+                        Console.WriteLine("\n\tSelectionsort:");
+                        foreach (var item in vector3)
+                        {
+                            Console.Write($"{item} - ");
+                        }
+
+                        break;
+
+                    case 2:
+                        //Sorting Bubblesort
+
+                        sorting.Bubblesort(vector1);
+
+                        //View
+                        Console.WriteLine("\n\tBubblesot:");
+                        foreach (var item in vector1)
+                        {
+                            Console.Write($"{item} - ");
+                        }
+                        break;
+
+                    case 3:
+                        //Sorting Insertionsort
+                        sorting.Insertionsort(vector2);
+
+                        //View
+                        Console.WriteLine("\n\tInsertionsort:");
+                        foreach (var item in vector2)
+                        {
+                            Console.Write($"{item} - ");
+                        }
+                        break;
+
+                    case 4:
+                        //Sorting Selectionsort
+                        sorting.Selectionsort(vector3);
+
+                        //View
+                        Console.WriteLine("\n\tSelectionsort:");
+                        foreach (var item in vector3)
+                        {
+                            Console.Write($"{item} - ");
+                        }
+                        break;
+
+                    case 5:
+                        Stopwatch sw = new Stopwatch();
+
+                        sw.Start();
+                        sorting.Bubblesort(vector1);
+                        sw.Stop();
+                        Console.WriteLine($"Bubble: {sw.Elapsed.ToString()}");
+                        sw.Start();
+                        sorting.Insertionsort(vector2);
+                        sw.Stop();
+                        Console.WriteLine($"Insertion: {sw.Elapsed.ToString()}");
+                        sw.Start();
+                        sorting.Selectionsort(vector3);
+                        sw.Stop();
+                        Console.WriteLine($"Selection: {sw.Elapsed.ToString()}");
+                        break;
+                }
+            }
+
+        }
+
+    }
+}
+
---
+++ simple_sorting/Sorting.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+using System.Threading.Tasks;
+
+
+
+namespace simple_sorting
+{
+    class Sorting
+    {
+        public void Bubblesort(int[] vet)
+        {
+            bool change = false;
+            for (int j = 0; j < vet.Length; j++)
+            {
+                change = false;
+                for (int i = 0; i < vet.Length - 1 - j; i++)
+                {
+                    if (vet[i] > vet[i + 1])
+                    {
+                        int ass = vet[i];
+                        vet[i] = vet[i + 1];
+                        vet[i + 1] = ass;
+                        change = true;
+                    }
+                }
+                if (!change)
+                    return;
+            }
+        }
+        public void Insertionsort(int[] vet)
+        {
+            for (int i = 0; i < vet.Length; i++)
+            {
+                int ass = vet[i];
+                int index = i;
+                while (index > 0 && ass < vet[index - 1])
+                {
+                    vet[index] = vet[index - 1];
+                    index--;
+                }
+                vet[index] = ass;
+            }
+
+        }
+        public void Selectionsort(int[] vet)
+        {
+            for (int j = 0; j < vet.Length; j++)
+            {
+                int lowerIndex = j;
+                for (int i = j + 1; i < vet.Length; i++)
+                {
+                    if (vet[i] < vet[lowerIndex])
+                    {
+                        lowerIndex = i;
+                    }
+                }
+                int ass = vet[j];
+                vet[j] = vet[lowerIndex];
+                vet[lowerIndex] = ass;
+            }
+
+        }
+    }
+
+}
+
