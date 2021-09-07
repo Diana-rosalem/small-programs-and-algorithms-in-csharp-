a1572a141232ba54cd8c553c48be6aae3f25b821
Diana <61847738+Diana-rosalem@users.noreply.github.com>
2021-09-06 22:26:43 -0300

    add aesthetic upgrades

--- numRandom/Program.cs
+++ numRandom/Program.cs
@@ -6,9 +6,24 @@
     {
         static void Main(string[] args)
         {
-            Console.Write("\n === Solver's game: there is a secret number, try to guess it. Be careful, you only have 16 chances!!! ===");
-            string name;
-            Console.Write("\n\t Enter your name: ");
+            Console.ForegroundColor = ConsoleColor.Green; 
+            
+            Console.Write("\n\t\t      ==== Solver's game  ====");
+            Console.Write("\n     ______________");
+            Console.Write("\n    /  ()      () /|");
+            Console.Write("\n   /             / |");
+            Console.Write("\n  /  ()     ()  /()| \t There is a secret number, try to guess it. ");
+            Console.Write("\n +—————————————+   | \t Be careful, you only have 16 chances!!!");
+            Console.Write("\n |             |   |");
+            Console.Write("\n |             |   |");
+            Console.Write("\n |     ( )     |()/ \t\t  GOOD LUCK");
+            Console.Write("\n |             | / ");
+            Console.Write("\n +_____________+/ ");
+
+
+        string name;
+            Console.ResetColor();
+            Console.Write("\n\n\t Enter your name: ");
             name = Console.ReadLine();
 
             for (int tries = 16; tries > 0; tries--)
@@ -23,30 +38,41 @@
 
                 if (guess == 10)
                 {
-                    Console.WriteLine($"\n\t\t Congratulations {name}, you got it right!!");
+                Console.ForegroundColor = ConsoleColor.Yellow;
+                Console.WriteLine("\t\t    - ****** -\n");
+                Console.WriteLine("\t\t  --  ******  --\n");
+                Console.WriteLine("\t\t     - **** -\n");
+                Console.WriteLine("\t\t        **  \n");
+                Console.WriteLine("\t\t        **  \n");
+                Console.WriteLine("\t\t      ******\n");
+                Console.ResetColor();
+                Console.WriteLine($"\n\t\t Congratulations {name}, you got it right!!");
+
                     break;
                 }
 
                 else
                 {
-                    Console.WriteLine($"\n\t You missed {name}, try again!!");
-                    if (11 <= tries && tries <= 13)
-                    {
-                        Console.WriteLine("\n\t Hint 1: try a number between 1 and 1000 ");
-                    }
-                    else if (6 <= tries && tries <= 8)
-                    {
-                        Console.WriteLine("\n\t Hint 2: try a number between 1 and 100");
-                    }
-                    else if (tries <= 3)
-                    {
-                        Console.WriteLine("\n\t Hint 3: try a number between 1 and 10 ");
-                    }
-                    else if (tries == 0)
-                    {
-                        Console.WriteLine("\n\t Your attempts are over ");
-                    }
+                         Console.WriteLine($"\n\t You missed {name}, try again!!");
+                        if (11 <= tries && tries <= 13)
+                        {
+                            Console.WriteLine("\n\t Hint 1: try a number between 1 and 1000 ");
+                        }
+                        else if (6 <= tries && tries <= 8)
+                        {
+                            Console.WriteLine("\n\t Hint 2: try a number between 1 and 100");
+                        }
+                        else if (tries <= 3 )
+                        {
+                            Console.WriteLine("\n\t Hint 3: try a number between 1 and 10 ");
+                        }
+                        else if ( tries == 0)
+                        {
+                            Console.WriteLine("\n\t Your attempts are over ");
+                        }
 
+                    
+             
                 }
             }
         }
