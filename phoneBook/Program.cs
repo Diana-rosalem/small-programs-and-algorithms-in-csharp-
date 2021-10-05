2d448547c03c6254544d1e02c72be956145a62da
Diana <61847738+Diana-rosalem@users.noreply.github.com>
2021-10-05 20:47:10 -0300

    add phoneBook program

---
+++ phoneBook/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+
+
+class Program
+{
+    public static void Menu()
+    {
+        Console.WriteLine("\n===================== MENU =====================");
+        Console.WriteLine("--> Select an option  ");
+        Console.WriteLine("Enter 1 to add a new phone number");
+        Console.WriteLine("Enter 2 to search the phone number");
+        Console.WriteLine("Enter 3 to show the complete Phone Book");
+        Console.WriteLine("Enter 4 to remove the phone number");
+        Console.WriteLine("Enter 5 exit");
+        Console.WriteLine("================================================\n");
+        Console.Write("Enter your choice:");
+    }
+    static void Main(string[] args)
+    {
+        int op;
+        phoneBook pb1 = new phoneBook();
+
+        do
+        {
+            Console.Clear();
+            Menu();
+            op = int.Parse(Console.ReadLine());
+            
+
+            switch (op)
+            {
+                case 1:
+                    Console.Clear();
+                    pb1.addPhoneNumber();
+                    break;
+                case 2:
+                    Console.Clear();
+                    pb1.search();
+                    break;
+                case 3:
+                    Console.Clear();
+                    pb1.allBook();
+                    break;
+                case 4:
+                    Console.Clear();
+                    pb1.remove();
+                    break;
+
+            }
+        } while (op != 5);
+
+
+    }
+}
+
---
+++ phoneBook/phoneBook.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+
+    class phoneBook
+    {
+    string name;
+    int number;
+    private Dictionary<string, int> pBook = new Dictionary<string, int>();
+
+        
+        public void addPhoneNumber()
+        {
+        
+                Console.Write("Enter the name:");
+                name = Console.ReadLine();
+                Console.Write("Enter the phone number: ");
+                number = int.Parse(Console.ReadLine());
+
+
+                pBook.Add(name, number);
+                Console.WriteLine("\nsuccessfully added, press enter to continue");
+                Console.ReadLine();
+
+
+                name = "";
+                number = 0;
+      
+        }
+
+        public void search()
+        {
+            string searchName;
+            Console.Write("Enter the name:");
+            searchName = Console.ReadLine();
+
+            int searchNumber;
+                if (pBook.TryGetValue(searchName, out searchNumber))
+                {
+                    Console.WriteLine($"{searchName}:{searchNumber}");
+                    Console.WriteLine("\npress enter to continue");
+                    Console.ReadLine();
+                }
+                else
+                {
+                    Console.WriteLine("Could not find the number for the specified name, press enter ");
+                    Console.ReadLine();
+                }
+        }
+        public void allBook()
+        {
+            int count;
+            count = pBook.Count;
+            Console.WriteLine($"\nThere are {count} registered numbers.");
+
+            foreach(KeyValuePair<string, int> item in pBook)
+            {
+                Console.WriteLine("\nName: {0}, Number: {1}", item.Key, item.Value);
+            }
+            Console.WriteLine("\npress enter to continue");
+            Console.ReadLine();
+
+        }
+
+        public void remove() 
+        {
+            string RemoveName;
+            Console.Write("Enter the name:");
+            RemoveName = Console.ReadLine();
+            if(pBook.ContainsKey(RemoveName) == true)
+            {
+                pBook.Remove(RemoveName);
+                Console.WriteLine("\nsuccessfully removed, press enter to continue");
+                Console.ReadLine();
+            }
+            else
+            {
+                Console.WriteLine("Could not find the specified name");
+                Console.WriteLine("\npress enter to continue");
+                Console.ReadLine();
+            }
+        
+
+        }
+        
+       
+
+}
+
